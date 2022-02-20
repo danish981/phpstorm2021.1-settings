4c4b637bcb9e5d95303ed1dea24eab9bbfef3133
danish mehmood <danishmehmood981@gmail.com>
2022-02-20 19:30:16 +0500

    first commit

---
+++ consoles/.history/db/e41e7ffa-dc13-465b-89bb-805d16b6159e.sql
@@ -0,0 +1,11 @@
+create database laravel9app;
+;-- -. . -..- - / . -. - .-. -.--
+use laravel9app;
+;-- -. . -..- - / . -. - .-. -.--
+show tables;
+;-- -. . -..- - / . -. - .-. -.--
+show databases;
+;-- -. . -..- - / . -. - .-. -.--
+select * from COLUMN_PRIVILEGES;
+;-- -. . -..- - / . -. - .-. -.--
+select * from migrations;
